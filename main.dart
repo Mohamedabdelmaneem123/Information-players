a0ac0fb202ca75a63ce57f9347f0852d847e1d44
Mohamed Abdel monem Ahmed <102558379+Mohamedabdelmaneem123@users.noreply.github.com>
2023-12-23 19:28:21 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,34 +1,35 @@
 
-import 'package:application/layout/mistake.dart';
-import 'package:application/loogin.dart';
-import 'package:application/moduals/Register_screen/Register_Screen.dart';
-import 'package:application/moduals/bmi_screen/Bmi_screen.dart';
-import 'package:application/Mohamed_salah_screen.dart';
-import 'package:application/moduals/Result/Result_screen.dart';
-import 'package:application/moduals/users/UsersScreen.dart';
-import 'package:application/game_X_O.dart';
-import 'package:application/moduals/login/login_desgin.dart';
-import 'package:application/you_can.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import 'cgpt.dart';
 
-import 'moduals/counter_screen/Counter_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  Bloc.observer = MyBlocObserver();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
-      home: Counter(),
+      theme: ThemeData(appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+        titleTextStyle: TextStyle(
+          color: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+        elevation: 0.0,
+
+
+      )),
+      home: info(),
     );
   }
 }
